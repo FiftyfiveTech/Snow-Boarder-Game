244abe32d14fa0721a8135d7163fd020be2059a2
AmanGupta1706 <72645754+AmanGupta1706@users.noreply.github.com>
2021-11-12 19:03:05 +0530

    Add files via upload

---
+++ Scripts/CrashDetector.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
